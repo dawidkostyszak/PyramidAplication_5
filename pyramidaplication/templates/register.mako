d5df7c1b178e9e18ae282615d197fc2b652905f8
Dawid Kostyszak <d.kostyszak@gmail.com>
2013-07-12 11:37:50 +0200

    add registry

--- pyramidaplication/templates/register.mako
+++ pyramidaplication/templates/register.mako
@@ -2,10 +2,16 @@
 <div class="middle">
     <div class="form_login">
         <div class="head_login">Register</div>
-        <form method="post" action="/login">
-            <input class="input_text" type="text" value="login"/>
-            <input class="input_text" type="password" value="password"/>
-            <input class="input_text" type="password" value="confirm_password"/>
+
+        <form method="post" action="/register">
+            % if message == None and error != None:
+                <p style="color: red">${error}</p>
+            % elif message != None and error == None:
+                <p style="color: green">${message}</p>
+            % endif
+            <input class="input_text" type="text" value="login" name="login"/>
+            <input class="input_text" type="password" value="password" name="password"/>
+            <input class="input_text" type="password" value="confirm_password" name="confirm_password"/>
             <button class="btn btn-primary" type=submit>Register</button>
         </form>
     </div>
--- pyramidaplication/views.py
+++ pyramidaplication/views.py
@@ -27,7 +27,7 @@ def search_result_view(request):
     data = request.GET.get('item')
 
     if not data:
-        return HTTPFound('/')
+        return {'error': 'no data'}
 
     allegro_state = nokaut_state = 'price'
 
@@ -55,6 +55,7 @@ def search_result_view(request):
             nokaut_state = 'price win'
 
     response = {
+        'error': None,
         'product': data,
         'allegro_price_state': allegro_state,
         'allegro_price': allegro_price,
@@ -87,9 +88,39 @@ def login_view(request):
 @view_config(route_name='register',
              renderer='pyramidaplication:templates/register.mako')
 def register_view(request):
+    message = None
+    error = None
+
     if request.method == 'POST':
-        print request.POST.get('login'), request.POST.get('password'), request.POST.get('confirm_password')
-    return {}
+        login = request.POST.get('login')
+        password = request.POST.get('password')
+        conf_password = request.POST.get('confirm_password')
+        #import ipdb; ipdb.set_trace()
+
+        if not login:
+            error = 'Please add login'
+        elif not password:
+            error = 'Please add password'
+        elif not conf_password:
+            error = 'Please add confirm password'
+
+        if error:
+            return {'message': message, 'error': error}
+
+        log = DBSession.query(User).filter_by(login=login).first()
+
+        if log:
+            error = 'Login already in use please try another.'
+        elif len(password) < 8:
+            error = 'Password to short. Need 8 characters.'
+        elif password != conf_password:
+            error = 'Confirm password is different than password.'
+        else:
+            message = 'Login registered successfully.'
+            user = User(login=login, password=password)
+            DBSession.add(user)
+
+    return {'message': message, 'error': error}
 
 
 @view_config(
